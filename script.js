ed7ea32fa5460c7c77d79641aebc67a6ca3793ab
Mr-y-Mrs-deBail <142449717+Mr-y-Mrs-deBail@users.noreply.github.com>
2025-12-13 13:31:08 -0600

    MV

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 function getNextChristmas() {
     var now = new Date();
     var year = now.getFullYear();
-    var nextChristmas = new Date(year, 11, 25); // los meses en js son 0 a 11, diciembre es 11
+    var nextChristmas = new Date(year, 11, 25);
     
     if (now > nextChristmas) {
         nextChristmas.setFullYear(year + 1);
@@ -11,77 +11,85 @@ function getNextChristmas() {
     return nextChristmas.getTime();
 }
 
-// Configuración inicial del contador
 var countDownDate = getNextChristmas();
 var lastDays, lastHours, lastMinutes, lastSeconds;
+var x;
 
-// Actualización del contador cada segundo
-var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
+function startCountdown() {
+    x = setInterval(function() {
+        var now = new Date().getTime();
+        var distance = countDownDate - now;
+                
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                
+        if (distance < 0) {
             
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-            
-// Verificación de si el contador ha llegado a cero
-    if (distance < 0) {
-        clearInterval(x);
-        countDownDate = getNextChristmas();
-        distance = countDownDate - now;
-        days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            const boton3 = document.getElementById("botón3");
 
-        //Sorpesa disponible al terminar la cuenta regresiva
-        
-        var boton = document.getElementById("botón3");
-        boton.disabled = false;
-        boton.classList.add("active"); 
-        boton.onclick = function() {
-            Swal.fire({
-                title: "¡Merry Christmas Mi Amor🌟!",
-                html: "Tu lindo regalo de Navidad soy YO🎀, pero tengo otra linda sorpresa para ti mi amor🎁. <br> Espero que te súper encante y ojalá la próxima Navidad sí podamos pasarla juntos mi todo. <br> ¡Feliz Navidad, mi vida🎄! <br> Recuerda que lo más lindo es que seguimos juntos, otro año más ¡Uyyyyyy! y estaremos juntos por toda la eternidad mi amor! <br> ❄️Te amoOOOOOOOOOO☃️ y siempre te amaré cada milisegundo más y más❤️.",      
-                position: "center",
-                background: "black",
-                color: "white",
-                confirmButtonText: 'Continuar',
-                confirmButtonColor: 'purple',
-                imageUrl: 'img/MV2.png',
-                imageWidth: '250px',
-                imageAlt: 'mv',
-                allowOutsideClick: false,
-                customClass: {
-                    title: 'titulo01'
-                }
-            })
-            .then((willContinue) => {
-                if (willContinue) {
-                    window.location.href = '//mr-y-mrs-debail.github.io/Merry-Christmas-Amor/';
-                }
-            });
-        };
-    }
-  
-    if (days !== lastDays) {
-        document.getElementById("days").innerHTML = days;
-        lastDays = days;
-    }
-    if (hours !== lastHours) {
-        document.getElementById("hours").innerHTML = hours;
-        lastHours = hours;
-    }
-    if (minutes !== lastMinutes) {
-        document.getElementById("minutes").innerHTML = minutes;
-        lastMinutes = minutes;
-    }
-    if (seconds !== lastSeconds) {
-        document.getElementById("seconds").innerHTML = seconds;
-        lastSeconds = seconds;
-    }
-}, 1000);
+            boton3.querySelector('strong').textContent = 'Sorpresa Disponible!';
+            boton3.disabled = false;
+            
+            boton3.classList.remove('sorpresa3'); 
+            boton3.classList.add('sorpresa2', 'active'); 
+
+            const boton4 = document.getElementById("botón4");
+            boton4.style.display = 'block'; 
+
+            boton3.onclick = function() {
+                Swal.fire({
+                    title: "¡Feliz Navidad Mi Amor!🎄",
+                    html: "Mi deseo más grande esta Navidad y todos los días🌟 es tenerte siempre cerca y siempre a mi lado Mi Todo!🎁✨", 
+                    position: "center",
+                    background: "black",
+                    color: "white",
+                    confirmButtonText: 'Continuar',
+                    confirmButtonColor: 'purple',
+                    imageUrl: 'img/MV3.png',
+                    imageWidth: '250px',
+                    imageAlt: 'mv',
+                    allowOutsideClick: false,
+                    customClass: {
+                        title: 'titulo01'
+                    }
+                })
+                .then((willContinue) => {
+                    if (willContinue) {
+                        window.location.href = '//mr-y-mrs-debail.github.io/Merry_Christmas2025/';
+                    }
+                });
+            };
+
+            clearInterval(x);
+            countDownDate = getNextChristmas();
+
+            startCountdown(); 
+            
+            return;
+        }
+    
+        if (days !== lastDays) {
+            document.getElementById("days").innerHTML = days;
+            lastDays = days;
+        }
+        if (hours !== lastHours) {
+            document.getElementById("hours").innerHTML = hours;
+            lastHours = hours;
+        }
+        if (minutes !== lastMinutes) {
+            document.getElementById("minutes").innerHTML = minutes;
+            lastMinutes = minutes;
+        }
+        if (seconds !== lastSeconds) {
+            document.getElementById("seconds").innerHTML = seconds;
+            lastSeconds = seconds;
+        }
+    }, 1000);
+}
+
+startCountdown();
 
 //Music <3 ----------------------------------------
 class musicPlayer {
@@ -99,8 +107,8 @@ class musicPlayer {
         let controlPanelObj = this.controlPanel;
         let infoBarObj = this.infoBar;
         
-        Array.from(controlPanelObj.classList).find(function(element){   
-            return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');       
+        Array.from(controlPanelObj.classList).find(function(element){   
+            return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');       
         });
 
         Array.from(infoBarObj.classList).find(function(element){
